59aae0b6e6e4c2ed311c8ae19c4a9beeca511edc
luoying <342231335@qq.com>
2023-01-29 22:15:19 +0800

    first

---
+++ scripts/build.js
@@ -0,0 +1,30 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import eslint from '@rollup/plugin-eslint';
+import pkg from "../package.json";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" },
+      {
+        file: pkg.browser,
+        name: pkg.name,
+        format: "umd",
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        exclude: "node_modules/**",
+        typescript: require("typescript"),
+        useTsconfigDeclarationDir: true,
+      }),
+      eslint()
+    ],
+  },
+];
